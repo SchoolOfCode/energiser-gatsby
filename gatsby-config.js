79896d87b1ea55945aa1672bb7028c6ba7801342
ben <benlee.thatsme@gmail.com>
2019-11-22 13:24:43 +0000

    icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         background_color: `#008dd6`,
         theme_color: `#008dd6`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/e.png`, // This path is relative to the root of the site.
       },
     },
     {
